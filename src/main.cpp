b9b6bed09bbad357f6912fff62b3cf0c7154cfd4
carlos-sweb <c4rl0sill3sc4@gmail.com>
2025-07-13 16:14:10 -0400

    improve

--- src/main.cpp
+++ src/main.cpp
@@ -18,33 +18,32 @@ using namespace std;
 #include <filesystem>
 #include <fstream>
 namespace fs = std::filesystem;
-
 #define URl_BCENTRAL "https://si3.bcentral.cl//Indicadoressiete/secure/Indicadoresdiarios.aspx"
 
-string cleanValue(string valueUF){
-	if( valueUF == "ND"){
+const char* meses[] = {
+    	"Enero","Febrero","Marzo", 
+    	"Abril", "Mayo", "Junio",
+		"Julio","Agosto","Septiembre",
+		"Octubre","Noviembre","Diciembre"
+	};
+
+
+const string cleanValue(string valueUF){
+	if(valueUF == "ND")
 		return "0.0";
-	}else{
-		string output="";	
-		for(int i = (valueUF.length()-1);i >= 0;i--){
-			string val = string(1,valueUF.at(i));
-			if( val != "."){
-				if(val == ","){
-					output.insert(0,".");	
-				}else{
-					output.insert(0,val);	
-				}			
-			}
-		}
-		return output;
-	}		
+	string output="";	
+	for(int i = (valueUF.length()-1);i >= 0;i--){
+		const string val = string(1,valueUF.at(i));		
+		if(val!="."){output.insert(0, val == "," ? "." : val );}
+	}
+	return output;		
 }
 
-string int_CLP(int cal){
+const string int_CLP(int cal){
 	string output="";
-	string body = to_string(cal);
+	const string body = to_string(cal);
 	int position_separate=0;
-	for(int i = (body.size()-1); i >= 0; i--){
+	for(int i = (body.size()-1); i >= 0; i--){		
 		if(position_separate == 3){
 			output.insert(0,".");
 			position_separate=1;
@@ -56,10 +55,10 @@ string int_CLP(int cal){
 	return output;
 }
 
-std::string blockBase(
-	string color,
-	string text,
-	int rows ,
+const std::string blockBase(
+	const string color,
+	const string text,
+	int rows,
 	int text_indent=1
 	){	
 	string output = "|";
@@ -78,14 +77,14 @@ std::string blockBase(
 	}	
 	return output;
 }
-std::string blockLeftGreen(string text,int rows ,int text_indent=1){	
+const std::string blockLeftGreen(string text,int rows ,int text_indent=1){	
  	return blockBase("\033[32m",text,rows,text_indent);
 }
-std::string blockLeftYellow(string text,int rows ,int text_indent=1){	
+const std::string blockLeftYellow(string text,int rows ,int text_indent=1){	
  	return blockBase("\033[33m",text,rows,text_indent);
 }
 
-std::string getText(lxb_dom_element_t *element){
+const std::string getText(lxb_dom_element_t *element){
     std::string text_full = "";
     lxb_dom_node_t * node = (lxb_dom_node_t * ) element;
     lxb_dom_node_t * current_node = lxb_dom_node_first_child( node );
@@ -100,12 +99,9 @@ std::string getText(lxb_dom_element_t *element){
     return text_full;
 }
 
-string loadContentBCentral(){
-	string html="";	
-    auto url_bcentral = cpr::Url{URl_BCENTRAL};
-    auto response = cpr::Get(url_bcentral);
-    if(response.status_code == 200){ html += response.text;}
-    return html;
+const string loadContentBCentral(){	
+    auto response = cpr::Get(cpr::Url{URl_BCENTRAL});    
+    return response.status_code == 200 ? response.text : "";
 }
 
 string ById(string value, lxb_html_document_t * document , lxb_dom_element_t * body){
@@ -132,42 +128,41 @@ string ById(string value, lxb_html_document_t * document , lxb_dom_element_t * b
 	return getText(element);
 }
 
-// valgrind --leak-check=full --show-leak-kinds=all ./lexbor-cpp
+
+typedef struct{
+	float f_vl;
+	string s_vl;
+	bool exists = false;
+} vlBcentral;
+
+// valgrind --leak-check=full --show-leak-kinds=all ./build/indicadores
 int main(int argc, char * argv[]){
 
 	auto cmdl = argh::parser(argc, argv);
 
-	float f_uf;
-	string s_uf;
-	bool f_uf_exists = false;
-
-	float f_dolar;
-	string s_dolar;
-	bool f_dolar_exists = false;
-
-	float f_euro;
-	string s_euro;
-	bool f_euro_exists = false;
-
+	vlBcentral v_uf;		
+	vlBcentral v_dolar;
+	vlBcentral v_euro;	
+	
 	if( cmdl({"--uf" }) ){
-		s_uf = cmdl({"--uf" }).str();
-		cmdl("uf",1.0f) >> f_uf;
-		f_uf_exists = true;		
+		v_uf.s_vl = cmdl({"--uf" }).str();
+		cmdl("uf",1.0f) >> v_uf.f_vl;		
+		v_uf.exists = true;
 	}
 
 	if( cmdl({"--dolar" }) ){		
-		s_dolar = cmdl({"--dolar" }).str();
-		cmdl("dolar",1.0f) >> f_dolar;
-		f_dolar_exists = true;		
+		v_dolar.s_vl = cmdl({"--uf" }).str();
+		cmdl("uf",1.0f) >> v_dolar.f_vl;		
+		v_dolar.exists = true;
 	}
 
 	if( cmdl({"--euro" }) ){		
-		s_euro = cmdl({"--euro" }).str();
-		cmdl("euro",1.0f) >> f_euro;
-		f_euro_exists = true;		
+		v_euro.s_vl = cmdl({"--uf" }).str();
+		cmdl("uf",1.0f) >> v_euro.f_vl;		
+		v_euro.exists = true;		
 	}
 	
-	string html = loadContentBCentral();
+	const string html = loadContentBCentral();
 
 	//LEXBOR PARSER
 	size_t size;
@@ -180,10 +175,10 @@ int main(int argc, char * argv[]){
 	if (status != LXB_STATUS_OK) {exit(EXIT_FAILURE);}
 	body = lxb_dom_interface_element(document->body);
 	// search value from ID
-	string valueUF = ById("lblValor1_1",document,body);
-	string valueDolar = ById("lblValor1_3",document,body);
-	string valueEuro = ById("lblValor1_5",document,body);
-	string valueYen = ById("lblValor1_10",document,body);
+	const string valueUF = ById("lblValor1_1",document,body);
+	const string valueDolar = ById("lblValor1_3",document,body);
+	const string valueEuro = ById("lblValor1_5",document,body);
+	const string valueYen = ById("lblValor1_10",document,body);
 	// free document
 	if(document != NULL){lxb_html_document_destroy( document );}
 
@@ -191,47 +186,46 @@ int main(int argc, char * argv[]){
     std::tm* now = std::localtime(&t);
     cout << "\n";
     cout << " " <<termcolor::green << now->tm_mday << " de ";
-    const char* meses[] = {
-    	"Enero","Febrero","Marzo", 
-    	"Abril", "Mayo", "Junio",
-		"Julio","Agosto","Septiembre",
-		"Octubre","Noviembre","Diciembre"
-	};
+    
     cout << meses[now->tm_mon] << " " << (now->tm_year + 1900) << termcolor::reset << std::endl;
     
 	const string s_separate = 	" -----------------------------\n";
 	const int row_green = 12;
 	const int row_yellow = 12;
 	// row_green+row_yellow+5;	
-	if( f_uf_exists ){
-		cout << s_separate;	
-		cout <<" "<< blockLeftGreen(string("UF(").append(s_uf+")"), row_green) ;
-		int cal_uf = f_uf*std::stof(cleanValue(valueUF));
-		cout << blockLeftYellow(int_CLP(cal_uf), row_yellow);
-		cout << "|\n";		
+	if( v_uf.exists){
+		if(valueUF != "ND" ){
+			cout << s_separate;	
+			cout <<" "<< blockLeftGreen(string("UF(").append(v_uf.s_vl+")"), row_green) ;
+			int cal_uf = v_uf.f_vl*std::stof(cleanValue(valueUF));
+			cout << blockLeftYellow(int_CLP(cal_uf), row_yellow);
+			cout << "|\n";		
+		}		
 	}
-	if( f_dolar_exists ){
-		cout << s_separate;	
-		cout <<" "<< blockLeftGreen(string("Dolar(").append(s_dolar+")"), row_green) ;
-		int cal_dolar = f_dolar*std::stof(cleanValue(valueDolar));
-		cout << blockLeftYellow(int_CLP(cal_dolar), row_yellow);
-		cout << "|\n";		
+	if( v_dolar.exists){
+		// se podria agregar un mensaje 
+		// cunado el valor es ND para sabados y domingos
+		if(valueDolar != "ND"){
+			cout << s_separate;	
+			cout <<" "<< blockLeftGreen(string("Dolar(").append(v_dolar.s_vl+")"), row_green) ;
+			int cal_dolar = v_dolar.f_vl*std::stof(cleanValue(valueDolar));
+			cout << blockLeftYellow(int_CLP(cal_dolar), row_yellow);
+			cout << "|\n";		
+		}
 	}
-	if( f_euro_exists ){
+	if( v_euro.exists && valueEuro != "ND" ){
 		cout << s_separate;	
-		cout <<" "<< blockLeftGreen(string("Euro(").append(s_euro+")"), row_green) ;		
-		int cal_euro = f_euro*std::stof(cleanValue(valueEuro));
+		cout <<" "<< blockLeftGreen(string("Euro(").append(v_euro.s_vl+")"), row_green) ;		
+		int cal_euro = v_euro.f_vl*std::stof(cleanValue(valueEuro));
 		cout << blockLeftYellow(int_CLP(cal_euro), row_yellow);
 		cout << "|\n";		
 	}
 	cout << s_separate;
-
-	cout << " " <<blockLeftGreen("UF", row_green) << blockLeftYellow(valueUF,row_yellow)  << "|\n";
-	cout << " "<<blockLeftGreen("Dolar", row_green) << blockLeftYellow(valueDolar,row_yellow) <<"|\n";
-	cout << " "<<blockLeftGreen("Euro", row_green) << blockLeftYellow(valueEuro,row_yellow) <<"|\n";
-	cout << " "<<blockLeftGreen("Yen", row_green) << blockLeftYellow(valueYen,row_yellow) <<"|\n";
+	cout << " "<<blockLeftGreen("UF", row_green)<<blockLeftYellow(valueUF,row_yellow)<<"|\n";
+	cout << " "<<blockLeftGreen("Dolar", row_green)<<blockLeftYellow(valueDolar,row_yellow)<<"|\n";
+	cout << " "<<blockLeftGreen("Euro", row_green)<<blockLeftYellow(valueEuro,row_yellow)<<"|\n";
+	cout << " "<<blockLeftGreen("Yen", row_green)<<blockLeftYellow(valueYen,row_yellow)<<"|\n";
 	cout << s_separate << "\n";	
 
-//LEXBOR PARSER    
     return 0;
 }
