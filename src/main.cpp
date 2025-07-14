b2f1440bbc22b474588f0cb619df96773e32871f
carlos-sweb <c4rl0sill3sc4@gmail.com>
2025-07-13 23:13:31 -0400

    improve code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <strings.h>
 #include <iostream>
 #include <memory.h>
@@ -137,58 +138,85 @@ typedef struct{
 
 // valgrind --leak-check=full --show-leak-kinds=all ./build/indicadores
 int main(int argc, char * argv[]){
+	std::time_t t = std::time(nullptr);
+    std::tm* now = std::localtime(&t);
+	argh::parser cmdl(argc, argv);
+	vlBcentral v_uf,v_dolar,v_euro;
 
-	auto cmdl = argh::parser(argc, argv);
+	const string string_today_date = to_string(now->tm_mday) + "-" +to_string(now->tm_mon+1) + "-" + to_string((now->tm_year+1900));	
 
-	vlBcentral v_uf;		
-	vlBcentral v_dolar;
-	vlBcentral v_euro;	
-	
-	if( cmdl({"--uf" }) ){
+	/*
+	map<string,vlBcentral> bcentral_values;	
+	vector<string> values = {"uf","dolar","euro"};
+	for(auto s_vl:values){
+		if(cmdl({s_vl})){
+			bcentral_values[s_vl] = vlBcentral{.s_vl=s_vl,};
+			if(cmdl(s_vl,1.0f) >> bcentral_values[s_vl].f_vl){
+				bcentral_values[s_vl].exists=true;
+			}
+		}
+	}
+	*/
+
+	if(cmdl({"--uf"})){
 		v_uf.s_vl = cmdl({"--uf" }).str();
-		cmdl("uf",1.0f) >> v_uf.f_vl;		
-		v_uf.exists = true;
+		if(cmdl("--uf",1.0f) >> v_uf.f_vl){
+			v_uf.exists = true;
+		}	
+	}		
+	if(cmdl({"--dolar"})){
+		v_dolar.s_vl=cmdl({"--uf" }).str();
+		if(cmdl("uf",1.0f)>>v_dolar.f_vl){
+			v_dolar.exists=true;
+		}
 	}
-
-	if( cmdl({"--dolar" }) ){		
-		v_dolar.s_vl = cmdl({"--uf" }).str();
-		cmdl("uf",1.0f) >> v_dolar.f_vl;		
-		v_dolar.exists = true;
-	}
-
-	if( cmdl({"--euro" }) ){		
+	if(cmdl({"--euro" })){
 		v_euro.s_vl = cmdl({"--uf" }).str();
-		cmdl("uf",1.0f) >> v_euro.f_vl;		
-		v_euro.exists = true;		
+		if(cmdl("uf",1.0f) >> v_euro.f_vl){
+			v_euro.exists = true;
+		}
 	}
-	
+
+
 	const string html = loadContentBCentral();
 
-	//LEXBOR PARSER
+	/*
+	const char* home_dir = std::getenv("HOME");
+	 if (home_dir != nullptr) {
+	 	fs::path cache_path{"/home/sweb/.scrapy-indicadores/cache/13-7-2025.html"};
+	 }else{
+	 	fs::path cache_path{"./.scrapy-indicadores/cache/13-7-2025.html"};
+	 }
+
+	cout << cache_path.c_str() << "\n";
+	if(!fs::exists(cache_path)){
+		cout << "No existe \n";
+		fs::create_directories(cache_path.parent_path());
+		std::ofstream ofs(cache_path);
+    	ofs << html;
+    	ofs.close();
+	}	
+	*/
+
+
 	size_t size;
 	lxb_status_t status;        	 
 	lxb_html_document_t  * document;
-	lxb_dom_element_t    * body;
-		
-    document = lxb_html_document_create();
-	status = lxb_html_document_parse(document,( const lxb_char_t *) html.c_str(),html.length());
-	if (status != LXB_STATUS_OK) {exit(EXIT_FAILURE);}
+	lxb_dom_element_t    * body;		
+    document=lxb_html_document_create();
+	status=lxb_html_document_parse(document,(const lxb_char_t *)html.c_str(),html.length());
+	if(status!=LXB_STATUS_OK){exit(EXIT_FAILURE);}
 	body = lxb_dom_interface_element(document->body);
 	// search value from ID
-	const string valueUF = ById("lblValor1_1",document,body);
-	const string valueDolar = ById("lblValor1_3",document,body);
-	const string valueEuro = ById("lblValor1_5",document,body);
-	const string valueYen = ById("lblValor1_10",document,body);
+	const string valueUF=ById("lblValor1_1",document,body);
+	const string valueDolar=ById("lblValor1_3",document,body);
+	const string valueEuro=ById("lblValor1_5",document,body);
+	const string valueYen=ById("lblValor1_10",document,body);
 	// free document
-	if(document != NULL){lxb_html_document_destroy( document );}
-
-    std::time_t t = std::time(nullptr);
-    std::tm* now = std::localtime(&t);
+	if(document != NULL){lxb_html_document_destroy(document);}
     cout << "\n";
-    cout << " " <<termcolor::green << now->tm_mday << " de ";
-    
-    cout << meses[now->tm_mon] << " " << (now->tm_year + 1900) << termcolor::reset << std::endl;
-    
+    cout << " " <<termcolor::green << now->tm_mday << " de ";    
+    cout << meses[now->tm_mon] << " " << (now->tm_year + 1900) << termcolor::reset << endl;
 	const string s_separate = 	" -----------------------------\n";
 	const int row_green = 12;
 	const int row_yellow = 12;
@@ -226,6 +254,5 @@ int main(int argc, char * argv[]){
 	cout << " "<<blockLeftGreen("Euro", row_green)<<blockLeftYellow(valueEuro,row_yellow)<<"|\n";
 	cout << " "<<blockLeftGreen("Yen", row_green)<<blockLeftYellow(valueYen,row_yellow)<<"|\n";
 	cout << s_separate << "\n";	
-
     return 0;
 }
